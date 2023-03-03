4c2a89282277ea5ee180cc6af51fc35637e9226d
Andrew Mo <106348010+theandrewmo@users.noreply.github.com>
2023-03-03 09:47:46 -0700

    added styling and templates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from stories import story
+from stories import stories
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -9,14 +9,22 @@ debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def index():
-    """ Show homepage with form """
-    
-    return render_template('base.html', prompts=story.prompts)
+    """ Show homepage for story selection """
 
-@app.route('/story', methods=["POST"])
-def story_page():
+    return render_template('story-select.html', stories=stories)
+    
+
+@app.route('/form', methods=["POST"])
+def form():
+    """ Show form with prompts for chosen story instance"""
+
+    ind = int(request.form.get('stories'))
+    return render_template('form.html', prompts=stories[ind].prompts, ind=ind)
+
+@app.route('/story/<int:storyInd>', methods=["POST"])
+def story_page(storyInd):
     """ Show story """
 
     form_data = request.form
-    new_story = story.generate(form_data)
+    new_story = stories[storyInd].generate(form_data)
     return render_template('story.html', new_story=new_story)
--- stories.py
+++ stories.py
@@ -37,9 +37,29 @@ class Story:
 
 # Here's a story to get you started
 
-
-story = Story(
+story1 = Story(
     ["place", "noun", "verb", "adjective", "plural_noun"],
     """Once upon a time in a long-ago {place}, there lived a
        large {adjective} {noun}. It loved to {verb} {plural_noun}."""
 )
+
+story2 = Story(
+    ["adjective", "noun", "adjective", "verb_past_tense", "plural_noun", 'adjective', 'verb'],
+    """
+    Once upon a time, a {adjective} {noun} went on a {adjective} 
+    adventure and {verb_past_tense} many {plural_noun}. They were 
+    very {adjective} and couldn't wait to {verb} again. The end.
+    """
+)
+
+story3 = Story(
+    ["adjective", "noun", "adjective", "animal", "verb", "adjective", "noun", "verb", "verb_past_tense", "plural_noun"],
+    """
+    In a {adjective} {noun}, there lived a {adjective} {animal} 
+    who loved to {verb}. One day, they met a {adjective} {noun} 
+    who needed their help to {verb}. Together, they {verb_past_tense} 
+    and saved the day. From then on, they became the best of {plural_noun}.
+    """
+)
+
+stories = [story1, story2, story3]
